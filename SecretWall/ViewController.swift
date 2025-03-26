d45fac637ac44ffdea9aec2486553b0613126c94
Thalisson Gabriel da Silva Santos Melo <thalisson.melo@pxbank.com.br>
2025-03-26 16:16:29 -0300

    Initial Commit

---
+++ SecretWall/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SecretWall
+//
+//  Created by Thalisson Melo on 26/03/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SecretWallTests/SecretWallTests.swift
@@ -0,0 +1,17 @@
+//
+//  SecretWallTests.swift
+//  SecretWallTests
+//
+//  Created by Thalisson Melo on 26/03/25.
+//
+
+import Testing
+@testable import SecretWall
+
+struct SecretWallTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
